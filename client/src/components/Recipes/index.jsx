3766145c3c1c2d4689fbf571a3811c641ab7885e
outsiderintrospector <juansebcarv@outlook.com>
2021-12-06 12:28:31 -0300

    90% done

--- client/src/components/Recipes/index.jsx
+++ client/src/components/Recipes/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import styles from "./Recipes.module.css";
 
+const Recipe = ({ id, name, image, diets }) => {
 
-const Recipe = ({ id, name, image, diet }) => {
   return (
-    
     <NavLink to={`/recipe/${id}`} className={styles.navlink}>
       <div className={styles.container}>
         <h2 className={styles.h2}>{name}</h2>
         <img src={image} alt="" className={styles.img} />
-        <h3>{diet}</h3>
+
+        <div>
+          {diets.map(d=>  <span> {d.charAt(0).toUpperCase() + d.slice(1)}. </span>)}
+        </div>
       </div>
-       </NavLink>
+    </NavLink>
   );
 };
 
